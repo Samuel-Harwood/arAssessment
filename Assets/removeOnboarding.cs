643755fcada92959f6e89facb52844bbc0af9bc4
Samuel-Harwood <samuelharwood3823@gmail.com>
2023-11-19 20:37:05 +0000

    Some Stuff

--- Assets/removeOnboarding.cs
+++ Assets/removeOnboarding.cs
@@ -6,31 +6,61 @@ using UnityEngine.XR.ARFoundation;
 public class removeOnboarding : MonoBehaviour
 {
     private ARPlaneManager arPlaneManager;
-    public GameObject removeCanvas;
+    public GameObject scanRoom;
+    public GameObject tapToPlace;
+    public GameObject apple;
+    //public event Action<InteractableRegisteredEventArgs> interactableRegistered;
 
-    private void OnEnable()
+    public void Start()
     {
         // Get the ARPlaneManager component
         arPlaneManager = GetComponent<ARPlaneManager>();
+        scanRoom.SetActive(true);
+        tapToPlace.SetActive(false);
 
         // Enable plane detection
         if (arPlaneManager != null)
         {
             arPlaneManager.planesChanged += OnPlanesChanged;
         }
+
     }
 
+    public void update()
+    {
+        if (!scanRoom.activeSelf && arPlaneManager.trackables.count < 1)
+        {
+            tapToPlace.SetActive(true);
+            //arPlaneManager.planesChanged -= OnPlanesChanged;
+
+
+        }
+        if (arPlaneManager.trackables.count > 0)
+        {
+
+            tapToPlace.SetActive(false);
+
+        }
+    }
+   
+     
 
     private void OnPlanesChanged(ARPlanesChangedEventArgs args)
     {
+
         if (args.added.Count > 0)
         {
             // Disable the specified game object
-            if (removeCanvas != null)
-            {
-                removeCanvas.SetActive(false);
-            }
-        }
-    }
+            scanRoom.SetActive(false);
 
+        }
+        //if (args.removed.Count > 0)
+        //{
+        //    // Disable the specified game object
+        //    tapToPlace.SetActive(false);
+
+        //}
+
+
+    }
 }
